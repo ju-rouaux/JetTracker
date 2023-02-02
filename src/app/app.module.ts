3cfaa8efb2f4990921924ff4304b5f07861bc133
Julien Rouaux <ju.rouaux@gmail.com>
2023-02-02 11:37:08 +0100

    Création du premier component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommentaireComponent } from './components/commentaire/commentaire.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentaireComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/commentaire/commentaire.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commentaire',
+  templateUrl: './commentaire.component.html',
+  styleUrls: ['./commentaire.component.css']
+})
+export class CommentaireComponent {
+
+}
