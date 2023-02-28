a949bde54146cd52a89d7042a1bbbfd46853aa0b
Yunorga <yunorga@gmail.com>
2023-02-28 13:53:27 +0100

    Ajout boarding-pass

--- src/app/components/boarding-pass/boarding-pass.component.ts
+++ src/app/components/boarding-pass/boarding-pass.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-boarding-pass',
+  selector: 'boarding-pass',
   templateUrl: './boarding-pass.component.html',
   styleUrls: ['./boarding-pass.component.css']
 })
 export class BoardingPassComponent {
 
     num : number[];
-
+    test = "07:50 AM\n(date)";
     constructor() {
         this.num = [];
 
