4e2b0e77884cfa3a5a591d39800f7f6df3a11828
Yunorga <yunorga@gmail.com>
2023-03-08 14:33:08 +0100

    first commit

--- back/index.js
+++ back/index.js
@@ -1,14 +1,33 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const port = 3080;
+var admin = require("firebase-admin");
 
-app.use(cors());
+var serviceAccount = require("./serviceAccountKey.json");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:
+    "https://jettracker-ipm-default-rtdb.europe-west1.firebasedatabase.app",
 });
 
-app.listen(port, () => {
-  console.log("Le serveur tourne sur le port " + port);
+const db = admin.database();
+const ref = db.ref("path/s");
+
+// Envoyer des données à la base de données
+ref
+  .push({
+    name: "John Doe",
+    age: 30,
+    email: "john.doe@example.com",
+  })
+  .then(() => {
+    console.log("Données envoyées avec succès");
+  })
+  .catch((error) => {
+    console.error("Erreur lors de l'envoi des données :", error);
+  });
+
+ref.on("child_added", (snapshot, prevChildKey) => {
+  const newPost = snapshot.val();
+  console.log("Name: " + newPost.name);
+  console.log("Age: " + newPost.age);
+  console.log("Email: " + newPost.email);
 });
