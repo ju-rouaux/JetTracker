b25d25b8cab526e0bbe3dcd5a11ec16abac455c2
Yunorga <yunorga@gmail.com>
2023-03-20 17:37:58 +0100

    Ajout de l'utilisation du service perosnne au composant map

--- front/src/app/components/selecteur-personne/selecteur-personne.component.ts
+++ front/src/app/components/selecteur-personne/selecteur-personne.component.ts
@@ -4,11 +4,11 @@ import { PersonneService, Personne } from 'src/app/services/personne.service';
 @Component({
   selector: 'app-selecteur-personne',
   templateUrl: './selecteur-personne.component.html',
-  styleUrls: ['./selecteur-personne.component.css']
+  styleUrls: ['./selecteur-personne.component.css'],
 })
 export class SelecteurPersonneComponent {
   // Récupérer la liste des personnes
-  listePersonne : Personne[] = [];
+  listePersonne: Personne[] = [];
 
   constructor(
     // Initialiser le PersonneService
@@ -17,13 +17,12 @@ export class SelecteurPersonneComponent {
 
   async ngOnInit() {
     this.listePersonne = await this.personneService.getListePersonne();
-    console.log("Liste personnes " + this.listePersonne)
+    console.log('Liste personnes ' + this.listePersonne);
   }
 
   // Lorsqu'un élément de la liste est sélectionné
-  onSelected(idPersStr : string) {
+  onSelected(idPersStr: string) {
     let id = Number.parseInt(idPersStr);
     console.log(this.listePersonne[id]);
   }
-
 }
